bd77ce434e70fd63224012d560618e803fd86560
Assane AL Sudais <assanesidyalsudais2004@gmail.com>
2026-09-12 20:55:46 +0000

    fix: autoriser l acces aux pages sans connexion

--- routes/web.php
+++ routes/web.php
@@ -7,25 +7,24 @@
     use App\Controller\ReservationController;
     use App\Controller\AuthController;
     use App\Routing\RouteDefinition;
-    use App\Security\Role;
 
     return function (RouteCollector $r) {
         $r->addRoute('GET', '/login', RouteDefinition::publique([AuthController::class, 'showLogin']));
         $r->addRoute('POST', '/login', RouteDefinition::publique([AuthController::class, 'login']));
         $r->addRoute('POST', '/logout', RouteDefinition::authentifiee([AuthController::class, 'logout']));
 
-        $r->addRoute('GET', '/', RouteDefinition::authentifiee([SalleController::class, 'index']));
+        $r->addRoute('GET', '/', RouteDefinition::publique([SalleController::class, 'index']));
 
-        $r->addRoute('GET', '/salle', RouteDefinition::authentifiee([SalleController::class, 'index']));
-        $r->addRoute('GET', '/salle/create', RouteDefinition::protegee([SalleController::class, 'create'], Role::ADMIN));
-        $r->addRoute('POST', '/salle', RouteDefinition::protegee([SalleController::class, 'store'], Role::ADMIN));
-        $r->addRoute('GET', '/salle/{id:\d+}', RouteDefinition::authentifiee([SalleController::class, 'show']));
-        $r->addRoute('GET', '/salle/{id:\d+}/edit', RouteDefinition::protegee([SalleController::class, 'edit'], Role::ADMIN));
-        $r->addRoute('POST', '/salle/{id:\d+}/edit', RouteDefinition::protegee([SalleController::class, 'update'], Role::ADMIN));
+        $r->addRoute('GET', '/salle', RouteDefinition::publique([SalleController::class, 'index']));
+        $r->addRoute('GET', '/salle/create', RouteDefinition::publique([SalleController::class, 'create']));
+        $r->addRoute('POST', '/salle', RouteDefinition::publique([SalleController::class, 'store']));
+        $r->addRoute('GET', '/salle/{id:\d+}', RouteDefinition::publique([SalleController::class, 'show']));
+        $r->addRoute('GET', '/salle/{id:\d+}/edit', RouteDefinition::publique([SalleController::class, 'edit']));
+        $r->addRoute('POST', '/salle/{id:\d+}/edit', RouteDefinition::publique([SalleController::class, 'update']));
 
-        $r->addRoute('GET', '/reservation', RouteDefinition::authentifiee([ReservationController::class, 'index']));
-        $r->addRoute('GET', '/reservation/create', RouteDefinition::protegee([ReservationController::class, 'create'], Role::RESPONSABLE));
-        $r->addRoute('POST', '/reservation', RouteDefinition::protegee([ReservationController::class, 'store'], Role::RESPONSABLE));
-        $r->addRoute('GET', '/reservation/{id:\d+}', RouteDefinition::authentifiee([ReservationController::class, 'show']));
-        $r->addRoute('POST', '/reservation/{id:\d+}/cancel', RouteDefinition::authentifiee([ReservationController::class, 'cancel']));
+        $r->addRoute('GET', '/reservation', RouteDefinition::publique([ReservationController::class, 'index']));
+        $r->addRoute('GET', '/reservation/create', RouteDefinition::publique([ReservationController::class, 'create']));
+        $r->addRoute('POST', '/reservation', RouteDefinition::publique([ReservationController::class, 'store']));
+        $r->addRoute('GET', '/reservation/{id:\d+}', RouteDefinition::publique([ReservationController::class, 'show']));
+        $r->addRoute('POST', '/reservation/{id:\d+}/cancel', RouteDefinition::publique([ReservationController::class, 'cancel']));
     };
